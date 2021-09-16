f524b609c1aba1dd7c890881620ea2b2ee9e4f4b
saket2001 <saketchandorkar123@gmail.com>
2021-09-16 10:44:58 +0530

    changed auth redux

--- Store/auth.js
+++ Store/auth.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
   name: "adminAuth",
   initialState: {
-    status: true, // for logged in or not
+    status: false, // for logged in or not
     userData: null,
   },
   reducers: {
